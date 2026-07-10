e79a0301480abb00a9f6eba04d7a24942228ed39
Sivapprakash-Chinnaraj <159047650+Sivapprakash-Chinnaraj@users.noreply.github.com>
2026-07-10 21:29:12 +0530

    Enhance Docker run command for Streamlit app
    Updated the Docker run command to include container name and run the Streamlit app.

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull sivapprakash1634/summa_streamlit:latest
 
 # Run the Docker image as a container
-docker run -d -p 8501:8501 sivapprakash1634/summa_streamlit:latest
+docker run -d -p 8501:8501 --name summa_streamlit_app sivapprakash1634/summa_streamlit:latest streamlit run app.py --server.port=8501 --server.address=0.0.0.0
